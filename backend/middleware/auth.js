47c90574b02796b472b626fae474701778943121
Haider Iqbal <sardarhaider420@gmail.com>
2026-05-19 06:55:48 -0700

    Fixed auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,19 +1,19 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = function(req, res, next) {
-  // Header se token lo
+  
   const token = req.header('Authorization')?.replace('Bearer ', '');
   
-  // Token hai hi nahi
+ 
   if (!token) {
     return res.status(401).json({ message: 'Login first' });
   }
 
   try {
-    // Token verify karo
+    
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; // user info request mein save karo
-    next(); // aage jao
+    req.user = decoded; 
+    next(); 
   } catch {
     res.status(401).json({ message: 'Wrong Token ' });
   }
